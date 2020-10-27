094561e6cf88af5792738f45d6d904333169d4e8
Kjetil Svalestuen <kjetil.svalestuen@bekk.no>
2020-10-27 19:36:21 +0100

    Oppgave 1-4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import images from './data/images';
 
 function App() {
-  return null;
+    return (
+        <>
+            <Header />
+            {images.map((image) => (
+                <Image key={image.id} src={image.url} alt={image.description} />
+            ))}
+        </>
+    );
+}
+
+function Header() {
+    return <h1>Bekkstagram</h1>;
+}
+
+function Image({ src, alt }) {
+    return <img className="image" src={src} alt={alt} />;
 }
 
 export default App;
